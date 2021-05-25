82c752d711094707d908caa62b3f8c4778a65ba0
Esben Viskum <viskum@viskum.dk>
2021-05-25 15:36:01 +0200

    Initial Commit

---
+++ RatingView-Master/RatingView_MasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  RatingView_MasterApp.swift
+//  RatingView-Master
+//
+//  Created by Esben Viskum on 25/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct RatingView_MasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
